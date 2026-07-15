947c7cd67f9e28fa44762ecd7182448cdf890190
sandeshbhatta495 <bhattasandesh148@gmail.com>
2026-07-15 23:11:36 +0545

    MObile app development

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+void main() => runApp(const ProviderScope(child: MyApp()));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Livestock Tracker')),
+        body: FlutterMap(
+          options: const MapOptions(
+            initialCenter: LatLng(27.7089, 85.3206),
+            initialZoom: 13,
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
